c57069f2df00f6fa7d3480f029b3ce34b43471f9
Divyanshu800 <pathakdivyanshu800@gmail.com>
2024-12-30 23:49:36 +0530

    added rabbitmq setup in user service

--- user/index.js
+++ user/index.js
@@ -3,9 +3,11 @@ const userRoute = require("./routes/user.routes");
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const { connectDB } = require("./config/DB");
+const rabbitMq = require("./service/rabbit");
 
 const app = express();
 connectDB();
+rabbitMq.connect();
 
 app.use(cookieParser());
 app.use(express.json());
